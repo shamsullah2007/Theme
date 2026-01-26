e152be5667c0786e323766f8dabdead72e3df102
shamsullah2007 <shasmullah2007a@gamil.com>
2026-01-26 11:57:12 +0500

    nave are done

--- theme/header.php
+++ theme/header.php
@@ -16,6 +16,16 @@
         <a class="site-title" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
       <?php endif; ?>
     </div>
+    <?php
+      $aurora_avatar_url = '';
+      if ( is_user_logged_in() ) {
+          $aurora_avatar_url = function_exists( 'aurora_get_user_profile_image' )
+              ? aurora_get_user_profile_image( get_current_user_id() )
+              : get_avatar_url( get_current_user_id(), [ 'size' => 64 ] );
+      } else {
+          $aurora_avatar_url = get_avatar_url( 0, [ 'size' => 64 ] );
+      }
+    ?>
     <div class="mega-search">
       <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
         <label class="screen-reader-text" for="aurora-search">Search for:</label>
@@ -34,6 +44,17 @@
       </form>
     </div>
     <div class="header-actions">
+      <?php
+        $registration_page = get_page_by_path( 'registration' );
+        if ( ! $registration_page ) {
+            $registration_page = get_page_by_path( 'registeration' );
+        }
+        if ( ! $registration_page ) {
+            $registration_page = get_page_by_title( 'Registration' );
+        }
+        $register_url = $registration_page ? get_permalink( $registration_page ) : add_query_arg( 'action', 'register', wc_get_page_permalink( 'myaccount' ) );
+      ?>
+
       <?php if ( is_user_logged_in() ) : ?>
         <?php if ( current_user_can( 'manage_woocommerce_products' ) || current_user_can( 'manage_options' ) ) : ?>
           <a class="add-product-link" href="<?php echo esc_url( aurora_get_product_manager_url( 'add' ) ); ?>" title="<?php esc_attr_e( 'Add Product', 'aurora' ); ?>">
@@ -41,11 +62,17 @@
             <?php esc_html_e( 'Add Product', 'aurora' ); ?>
           </a>
         <?php endif; ?>
+        <a class="avatar-link" href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" title="<?php esc_attr_e( 'My Account', 'aurora' ); ?>">
+          <span class="avatar-circle"><img src="<?php echo esc_url( $aurora_avatar_url ); ?>" alt=""></span>
+        </a>
         <a class="account-link" href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" title="<?php esc_attr_e( 'My Account', 'aurora' ); ?>"><?php esc_html_e( 'Account', 'aurora' ); ?></a>
         <a class="logout-link" href="<?php echo esc_url( wp_logout_url( home_url( '/' ) ) ); ?>" title="<?php esc_attr_e( 'Logout', 'aurora' ); ?>"><?php esc_html_e( 'Logout', 'aurora' ); ?></a>
       <?php else : ?>
+        <a class="avatar-link" href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" title="<?php esc_attr_e( 'Login or Register', 'aurora' ); ?>">
+          <span class="avatar-circle"><img src="<?php echo esc_url( $aurora_avatar_url ); ?>" alt=""></span>
+        </a>
         <a class="login-link" href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" title="<?php esc_attr_e( 'Login', 'aurora' ); ?>"><?php esc_html_e( 'Login', 'aurora' ); ?></a>
-        <a class="register-link" href="<?php echo esc_url( add_query_arg( 'action', 'register', wc_get_page_permalink( 'myaccount' ) ) ); ?>" title="<?php esc_attr_e( 'Register', 'aurora' ); ?>"><?php esc_html_e( 'Register', 'aurora' ); ?></a>
+        <a class="register-link" href="<?php echo esc_url( $register_url ); ?>" title="<?php esc_attr_e( 'Register', 'aurora' ); ?>"><?php esc_html_e( 'Register', 'aurora' ); ?></a>
       <?php endif; ?>
       <a class="cart-link" href="<?php echo esc_url( wc_get_cart_url() ); ?>" title="<?php esc_attr_e( 'Shopping Cart', 'aurora' ); ?>">
         <span class="cart-icon">🛒</span>
